3997258f6b73d5623046116cbff15da3ec9400e0
Dwayne Camacho <dwayne.camacho1277@gmail.com>
2025-03-25 14:04:57 -0400

    added create device type job

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,6 @@
 from nautobot.apps.jobs import register_jobs
 from .hello_jobs import HelloJobs
+from .create_device_type import CreateDeviceType
 
 register_jobs(HelloJobs)
+register_jobs(CreateDeviceType)
---
+++ jobs/create_device_type.py
@@ -0,0 +1,135 @@
+import re
+from itertools import product
+from nautobot.apps.jobs import Job, register_jobs
+from nautobot.dcim.models import DeviceType, Manufacturer
+from nautobot.dcim.models.device_component_templates import InterfaceTemplate
+
+
+DEVICE_TYPES_YAML = [
+    """
+    manufacturer: Arista
+    model: DCS-7280CR2-60
+    part_number: DCS-7280CR2-60
+    u_height: 1
+    is_full_depth: true
+    comments: '[Arista 7280R Data Sheet](https://www.arista.com/assets/data/pdf/Datasheets/7280R-DataSheet.pdf)'
+    interfaces:
+        - pattern: "Ethernet[1-60]/[1-4]"
+          type: 100gbase-x-qsfp28
+        - pattern: "Management1"
+          type: 1000base-t
+          mgmt_only: true
+    """,
+    """
+    manufacturer: Arista
+    model: DCS-7150S-24
+    part_number: DCS-7150S-24
+    u_height: 1
+    is_full_depth: true
+    comments: '[Arista 7150 Data Sheet](https://www.arista.com/assets/data/pdf/Datasheets/7150S_Datasheet.pdf)'
+    interfaces:
+        - pattern: "Ethernet[1-24]"
+          type: 10gbase-x-sfpp
+        - pattern: "Management1"
+          type: 1000base-t
+          mgmt_only: true
+    """,
+]
+
+
+def expand_interface_pattern(pattern):
+    """
+    Expands an interface pattern like 'Ethernet[1-60]/[1-4]' into actual names.
+    
+    Supports:
+      - Single range: Ethernet[1-24] -> Ethernet1, Ethernet2, ..., Ethernet24
+      - Nested range: Ethernet[1-60]/[1-4] -> Ethernet1/1, Ethernet1/2, ..., Ethernet60/4
+    """
+    match = re.findall(r"\[([0-9]+)-([0-9]+)\]", pattern)
+    if not match:
+        return [pattern]  # No expansion needed, return as-is.
+
+    # Convert to lists of numbers
+    try:
+        ranges = [list(range(int(start), int(end) + 1)) for start, end in match]
+    except ValueError:
+        raise ValueError(f"Invalid range in pattern: {pattern}")
+
+    # Generate base name with placeholders
+    base_name = re.sub(r"\[[0-9]+-[0-9]+\]", "{}", pattern, count=len(ranges))
+
+    # Expand using cartesian product
+    return [base_name.format(*nums) for nums in product(*ranges)]
+
+
+
+def create_device_types(logger):
+    """
+    Create DeviceType objects from YAML definitions and add interfaces using InterfaceTemplate.
+    """
+
+    for device_yaml in DEVICE_TYPES_YAML:
+        data = yaml.safe_load(device_yaml)
+
+        manufacturer_name = data.pop("manufacturer", None)
+        if not manufacturer_name:
+            logger.error("Manufacturer not provided in YAML definition.")
+            continue
+        manufacturer_obj, _ = Manufacturer.objects.get_or_create(name=manufacturer_name)
+
+        model_name = data.pop("model", None)
+        if not model_name:
+            logger.error("Model not provided in YAML for manufacturer %s", manufacturer_name)
+            continue
+
+        # Create DeviceType
+        device_type_defaults = {
+            k: data[k] for k in ["part_number", "u_height", "is_full_depth", "comments"] if k in data
+        }
+        device_type_obj, created = DeviceType.objects.get_or_create(
+            manufacturer=manufacturer_obj,
+            model=model_name,
+            defaults=device_type_defaults,
+        )
+
+        if created:
+            device_type_obj.validated_save()
+            logger.info(f"DeviceType created: {device_type_obj}")
+        else:
+            logger.info(f"DeviceType already exists: {device_type_obj}")
+
+        # Add interfaces using InterfaceTemplate
+        for iface in data.get("interfaces", []):
+            pattern = iface.get("pattern")
+            iface_type = iface.get("type")
+            mgmt_only = iface.get("mgmt_only", False)
+
+            if not pattern or not iface_type:
+                logger.error(f"Invalid interface definition in {model_name}: {iface}")
+                continue
+
+            # Generate interfaces from range patterns
+            interface_names = expand_interface_pattern(pattern)
+            for iface_name in interface_names:
+                interface_template, created = InterfaceTemplate.objects.get_or_create(
+                    device_type=device_type_obj,
+                    name=iface_name,
+                    defaults={
+                        "type": iface_type,
+                        "mgmt_only": mgmt_only,
+                    },
+                )
+                if created:
+                    logger.info(f"Added interface {iface_name} ({iface_type}) to {model_name}")
+
+class CreateDeviceType(Job):
+    """
+    Calls on the previously defined functions to create device types with the interfaces
+    """
+
+    class Meta:
+        name = "Bulk add device types"
+
+    def run(self):
+        create_device_types(self.logger)
+
